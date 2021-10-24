11de4237aed78730c66028f33a21192687c6124a
wipaaa <ini.wipaaa@gmail.com>
2021-10-24 10:30:51 +0800

    Add index.php file at public

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * EProgress
+ *
+ * Sebuah aplikasi simple untuk mengelola data mahasiswa
+ * yang menjadi anggota UKM Progress
+ *
+ * @author wipaaa <ini.wipaaa@gmail.com>
+ * @copyright eprogress 2020
+ */
+
+require realpath(__DIR__ . '/../vendor/autoload.php');
+
+$app = new Progress\Base\Application();
