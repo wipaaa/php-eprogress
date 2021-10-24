a24066b52e888b2a39d38bf88ebfa20ed307d7b6
wipaaa <ini.wipaaa@gmail.com>
2021-10-24 10:32:35 +0800

    Add the main Application files

---
+++ src/Base/Application.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Progress\Base;
+
+use Progress\Container\Container;
+use Progress\Database\Database;
+use Progress\Database\Model;
+use Progress\Http\Input;
+use Progress\Http\Request;
+use Progress\Http\Response;
+use Progress\Router\Router;
+use Progress\Support\Cookie;
+use Progress\Support\Session;
+use Progress\View\View;
+
+class Application {
+	/**
+	 * Self class instance
+	 *
+	 * @var Progress\Base\Application
+	 */
+	private static $_instance;
+
+	/**
+	 * Container class instance
+	 *
+	 * @var Progress\Container\Container
+	 */
+	private $_container;
+
+	public function __construct() {
+		self::$_instance = $this;
+
+		define('BASE', 'http://localhost/progress');
+		define('DS', DIRECTORY_SEPARATOR);
+		define('ROOT', dirname(__DIR__, 2) . DS);
+		define('PATH_APP', realpath(ROOT . 'app') . DS);
+		define('PATH_CONFIG', realpath(ROOT . 'config') . DS);
+		define('PATH_LIB', realpath(ROOT . 'src') . DS);
+		define('PATH_PUBLIC', realpath(ROOT . 'public') . DS);
+		define('PATH_RESOURCE', realpath(ROOT . 'resource') . DS);
+		define('PATH_ROUTER', realpath(ROOT . 'router') . DS);
+
+		$this->_container = new Container();
+
+		$this->_container->bind(Application::class)
+			->setResolved($this)
+			->setSingleton(true);
+
+		$this->_container->bind(Container::class)
+			->setResolved($this->_container)
+			->setSingleton(true);
+
+		$this->_container->bind(Config::class)->setSingleton(true);
+		$this->_container->bind(Cookie::class)->setSingleton(true);
+		$this->_container->bind(Database::class)->setSingleton(true);
+		$this->_container->bind(Env::class)->setSingleton(true);
+		$this->_container->bind(Router::class)->setSingleton(true);
+		$this->_container->bind(Session::class)->setSingleton(true);
+		$this->_container->bind(View::class)->setSingleton(true);
+
+		$this->_container->bind(Input::class);
+		// $this->_container->bind(Model::class);
+		$this->_container->bind(Request::class);
+		$this->_container->bind(Response::class);
+
+		$this->_container->make(Router::class)->dispatch();
+	}
+
+	/**
+	 * Memakai class application secara static
+	 *
+	 * @return Progress\Container\Container
+	 */
+	public static function instance() {
+		return self::$_instance->_container;
+	}
+}
---
+++ src/Base/helpers.php
@@ -0,0 +1,251 @@
+<?php
+
+use Progress\Base\Application;
+use Progress\Base\Config;
+use Progress\Base\Env;
+use Progress\Facade\Container;
+use Progress\Router\Router;
+use Progress\Support\Cookie;
+use Progress\Support\Session;
+use Progress\View\View;
+
+if (!function_exists('app')) {
+	/**
+	 * Mengembalikan Application instance class
+	 *
+	 * @param string|null $_name
+	 * @return Progress\Base\Container|object
+	 */
+    function app(?string $_name = null) {
+        $app = Application::instance();
+
+        if ($_name !== null) {
+            return $app->make($_name);
+        }
+
+        return $app;
+    }
+}
+
+if (!function_exists('arrayForget')) {
+    /**
+     * Menghapus value dari sebuah array dengan notasi titik
+     *
+     * @param array $_array
+     * @param array|string $_key
+     */
+    function arrayForget(array &$_array, $_key) {
+        $original = &$_array;
+        $_keys = (array) $_key;
+
+        foreach ($_keys as $_key) {
+            if (isset($_array[$_key])) {
+                unset($_array[$_key]);
+                continue;
+            }
+
+            $parts = explode('.', $_key);
+            $_array = &$original;
+
+            while (count($parts) > 1) {
+                $part = array_shift($parts);
+
+                if (!isset($_array[$part]) || !is_array($_array[$part])) {
+                    continue 2;
+                }
+
+                $_array = &$_array[$part];
+            }
+
+            unset($_array[array_shift($parts)]);
+        }
+    }
+}
+if (!function_exists('arrayGet')) {
+    /**
+     * Mengambil value dari array dengan notasi titik
+     *
+     * @param array $_array
+     * @param string|null $_key
+     * @param mixed $default
+     * @return mixed
+     */
+    function arrayGet(
+		array $_array,
+		?string $_key,
+		mixed $default = null
+	) {
+        if ($_key === null) {
+            return $_array;
+        }
+
+        if (isset($_array[$_key])) {
+            return $_array[$_key];
+        }
+
+        $_keys = explode('.', $_key);
+
+        foreach ($_keys as $_key) {
+            if (!isset($_array[$_key])) {
+                return $default;
+            }
+
+            $_array = $_array[$_key];
+        }
+
+        return $_array;
+    }
+}
+
+if (!function_exists('arraySet')) {
+    /**
+     * Memasukkan value ke dalam array dengan notasi titik
+     *
+     * @param array $_array
+     * @param string|null $_key
+     * @param mixed $_value
+     * @return mixed
+     */
+    function arraySet(array &$_array, ?string $_key, mixed $_value) {
+        if ($_key === null) {
+            return $_array = $_value;
+        }
+
+        $_keys = explode('.', $_key);
+
+        while (count($_keys) > 1) {
+            $_key = array_shift($_keys);
+
+            // if the value is doesn't exists or not an array
+            // let assign them with an empty array so we
+            // can continue digging in and so on hehe
+            if (!isset($_array[$_key]) || !is_array($_array[$_key])) {
+                $_array[$_key] = [];
+            }
+
+            $_array = &$_array[$_key];
+        }
+
+        return $_array[array_shift($_keys)] = $_value;
+    }
+}
+
+if (!function_exists('config')) {
+	/**
+	 * Meresolve data config
+	 *
+	 * @param  string|null $_name
+	 * @param  mixed $_default
+	 * @return mixed
+	 */
+	function config(?string $_name = null, mixed $_default = null) {
+		$_config = Container::make(Config::class);
+
+		if (is_array($_name)) {
+			foreach ($_name as $_k => $_v) {
+				$_config->set($_k, $_v);
+			}
+		} else if (is_null($_name)) {
+			return $_config;
+		}
+
+		return $_config->get($_name, $_default);
+	}
+}
+
+if (!function_exists('env')) {
+	/**
+	 * Meresolve data env
+	 *
+	 * @param  string|null $_name
+	 * @param  mixed $_default
+	 * @return mixed
+	 */
+	function env(?string $_name = null, mixed $_default = null) {
+		$_env = Container::make(Env::class);
+
+		var_dump('hello world');
+
+		if (is_array($_name)) {
+			foreach ($_name as $_k => $_v) {
+				$_env->set($_k, $_v);
+			}
+		} else if (is_null($_name)) {
+			return $_env;
+		}
+
+		return $_env->get($_name, $_default);
+	}
+}
+
+if (!function_exists('cookie')) {
+	/**
+	 * Memanipulasi cookie
+	 *
+	 * @param  string|null $_name
+	 * @param  mixed $_default
+	 * @return mixed
+	 */
+	function cookie(?string $_name = null, mixed $_default = null) {
+		$_cookie = Container::make(Cookie::class);
+
+		if (is_array($_name)) {
+			foreach ($_name as $_k => $_v) {
+				$_cookie->set($_k, $_v);
+			}
+		} elseif (is_null($_name)) {
+			return $_cookie;
+		}
+
+		return $_cookie->get($_name, $_default);
+	}
+}
+
+if (!function_exists('route')) {
+	/**
+	 * Resolve route url
+	 *
+	 * @param  string $_name
+	 * @param  array $_args
+	 * @return string
+	 */
+	function route(string $_name, array $_args = []) {
+		return Container::make(Router::class)->resolve($_name, $_args);
+	}
+}
+
+if (!function_exists('session')) {
+	/**
+	 * Memanipulasi session
+	 *
+	 * @param  string|null $_name
+	 * @param  mixed $_default
+	 * @return mixed
+	 */
+	function session(?string $_name = null, mixed $_default = null) {
+		$_session = Container::make(Session::class);
+
+		if (is_array($_name)) {
+			foreach ($_name as $_k => $_v) {
+				$_session->set($_k, $_v);
+			}
+		} elseif (is_null($_name)) {
+			return $_session;
+		}
+
+		return $_session->get($_name, $_default);
+	}
+}
+
+if (!function_exists('view')) {
+	/**
+	 * Kompilasi view dengan standalone Blade template engine
+	 *
+	 * @param  string $_name
+	 * @param  array $_data
+	 * @return string
+	 */
+	function view(string $_name, array $_data = []) {
+		return Container::make(View::class)->make($_name, $_data);
+	}
+}
