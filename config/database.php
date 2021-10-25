54f98bae56a7e813c20ad0baca35ee9dd16961ae
wipaaa <ini.wipaaa@gmail.com>
2021-10-25 17:42:02 +0800

    Fixing database configuration

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-	'driver' => env('DB_DRVR', 'mysql'),
-	'name' => env('DB_NAME', 'ukk'),
-	'host' => env('DB_HOST', 'localhost'),
-	'port' => env('DB_PORT', 3306),
-	'user' => env('DB_USER', 'root'),
-	'pass' => env('DB_PASS', '')
+	'driver' => 'mysql',
+	'name' => 'project_progress',
+	'host' => 'localhost',
+	'port' => 3306,
+	'user' => 'root',
+	'pass' => ''
 ];
